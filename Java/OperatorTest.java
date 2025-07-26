53dd0ef1f3c506ec58b91c97168ecf1bba3fe001
Jaitheerth-A <jaitheerth21@gmail.com>
2025-07-26 18:22:20 +0530

    Update OperatorTest.java

--- Java/OperatorTest.java
+++ Java/OperatorTest.java
@@ -1,7 +1,14 @@
-public class OperatorTest {
-    static public void main(String args[])
-    {
-        
-    }
-    
+  class A { 
+int x; 
+A() 
+{ 
+this(5);
+System.out.print("A "); 
 }
+A(int x) 
+{ System.out.print("B "); } 
+} 
+class OperatorTest {
+    public static void main(String[] args) 
+    { A obj = new A(); } 
+    }
